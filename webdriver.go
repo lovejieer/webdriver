75480a9928346833b163916eadc3a0ad128b09e4
lovejieer <64838311+lovejieer@users.noreply.github.com>
2020-09-10 06:16:31 -0400

    Update webdriver.go

--- webdriver.go
+++ webdriver.go
@@ -1,9 +1,9 @@
 package webdriver
 
 import (
-	"encoding/json"
+
 	"fmt"
-	"github.com/parnurzeal/gorequest"
+
 )
 
 const (
@@ -24,35 +24,21 @@ type params map[string]interface{}
 type Session struct {
 	Id           string
 	Capabilities Capabilities
-	RequestUrl string
+	r           *Request
 }
 
-func (s *Session) Request(p params, method string, url string) (string ,[]error){
-	jsonParams, _ := json.Marshal(p)
-	request := gorequest.New()
-	if method == POST{
-		_, body, errs := request.Post(s.RequestUrl).
-		Send(string(jsonParams)).
-		End()
-		return body, errs
-	}else {
-		_, body, errs := request.Get(s.RequestUrl).
-			End()
-		return body, errs
-	}
-}
 
 
 func (s *Session) Url(url string) string{
 	ru := fmt.Sprintf("http://127.0.0.1:1234/session/%s/url",s.Id)
 	p := params{"url": url}
-	body,_ := s.Request(p,POST,ru)
+	body:= s.r.Post(ru,p)
 	return body
 }
 
 func (s *Session) Source() string{
 	ru := fmt.Sprintf("http://127.0.0.1:1234/session/%s/source",s.Id)
-	body,_ := s.Request(nil,GET,ru)
+	body := s.r.Get(ru)
 	return body
 }
 
