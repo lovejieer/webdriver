d34df71afb16c3eaa66be8a33660fa0e6a9ec6da
lovejieer <64838311+lovejieer@users.noreply.github.com>
2020-09-10 06:30:57 -0400

    Update firefoxdriver.go

--- firefoxdriver.go
+++ firefoxdriver.go
@@ -1,122 +1,49 @@
 package webdriver
 
 import (
-	"github.com/tidwall/gjson"
-	"io"
-	"os"
 
-	"os/exec"
+	"fmt"
 
-	"errors"
 )
 
-type FirefoxDriver struct {
-	*Session
-	Cap Capabilities
-	DriverPath    string
-	BinaryPath string
-	cmd     *exec.Cmd
-	LogPath string
-	LogFile string
+const (
+	POST = "POST"
+	GET = "GET"
+)
+type Webdriver interface {
+	Start() error
+	Stop() error
+	NewSession() (*Session, error)
 
-	logFile *os.File
-
-}
-
-func NewFirefoxDriver(f *FirefoxDriver) *FirefoxDriver{
-
-	return &FirefoxDriver{
-		Session: f.Session,
-		Cap: f.Capabilities,
-		DriverPath: "",
-		LogPath: "",
-		BinaryPath: "",
-		LogFile: "",
-	}
-}
-
-func (f *FirefoxDriver) Start() error{
-
-	csferr := "firefox driver start failed: "
-	if f.cmd != nil {
-		return errors.New(csferr + "firefoxdriver already running")
-	}
-
-	if f.LogPath != "" {
-		//check if log-path is writable
-		file, err := os.OpenFile(f.LogPath, os.O_WRONLY|os.O_CREATE, 0664)
-		if err != nil {
-			return errors.New(csferr + "unable to write in log path: " + err.Error())
-		}
-		file.Close()
-	}
-
-	var switches []string
-	switches = append(switches, "-b "+f.BinaryPath)
-	switches = append(switches, "-p 1234")
-
-	f.cmd = exec.Command(f.DriverPath, switches...)
-	stdout, err := f.cmd.StdoutPipe()
-	if err != nil {
-		return errors.New(csferr + err.Error())
-	}
-	stderr, err := f.cmd.StderrPipe()
-	if err != nil {
-		return errors.New(csferr + err.Error())
-	}
-	if err := f.cmd.Start(); err != nil {
-		return errors.New(csferr + err.Error())
-	}
-	if f.LogFile != "" {
-		flags := os.O_WRONLY | os.O_CREATE | os.O_TRUNC
-		f.logFile, err = os.OpenFile(f.LogFile, flags, 0640)
-		if err != nil {
-			return err
-		}
-		go io.Copy(f.logFile, stdout)
-		go io.Copy(f.logFile, stderr)
-	} else {
-		go io.Copy(os.Stdout, stdout)
-		go io.Copy(os.Stderr, stderr)
-	}
-
-	return nil
-}
-
-func (f *FirefoxDriver) Stop()error  {
-
-		f.r.Get("http://127.0.0.1:1234/shutdown")
-		err := f.cmd.Process.Signal(os.Interrupt)
-		if f.logFile != nil {
-			f.logFile.Close()
-		}
-
-		cmd := exec.Command("/usr/bin/pkill", "firefox")
-		err = cmd.Run()
-		if err != nil {
-		return err
-	}
-		cmd2 := exec.Command("/usr/bin/killall", "firefox")
-		err2 := cmd2.Run()
-		if err2 != nil {
-		return err
-
-	}
-		return nil
 
 }
 
-func (f *FirefoxDriver) NewSession() (*Session, error){
+type Capabilities map[string]interface{}
+type params map[string]interface{}
 
-		if err :=f.Start(); err != nil{
-			return nil, err
-		}
-		desired := f.Cap
-		required := Capabilities{}
-		p := params{"desiredCapabilities": desired, "requiredCapabilities": required}
-       body := f.r.Post("http://127.0.0.1:1234/session",p)
-		sid := gjson.Get(body, "value.sessionId")
-		f.Id = sid.String()
-       return f.Session,nil
+type Session struct {
+	Id           string
+	Capabilities Capabilities
+	r           *Request
+}
 
+
+
+func (s *Session) Url(url string) string{
+	ru := fmt.Sprintf("http://127.0.0.1:1234/session/%s/url",s.Id)
+	p := params{"url": url}
+	body:= s.r.Post(ru,p)
+	return body
+}
+
+func (s *Session) Source() string{
+	ru := fmt.Sprintf("http://127.0.0.1:1234/session/%s/source",s.Id)
+	body := s.r.Get(ru)
+	return body
+}
+
+func (s *Session) GetUrl() string{
+	ru := fmt.Sprintf("http://127.0.0.1:1234/session/%s/url",s.Id)
+	body := s.r.Get(ru)
+	return body
 }
